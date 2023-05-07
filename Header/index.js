9ad55a33e69a2cfd9b3e1f6d840376ef527707dc
Vendula Krajíčková <vendysacek@seznam.cz>
2023-05-07 18:28:14 +0200

    first commit

--- Header/index.js
+++ Header/index.js
@@ -1,6 +1,20 @@
 export const Header = (props) => {
 
+    // const {title} = props;
     const {title, links} = props;
+
+    // return `
+    //     <header>
+    //         <h1 class="site-title">${title}</h1>
+    //         <nav>
+    //             <a href="#">domů</a>
+    //             <a href="#">lekce</a>
+    //             <a href="#">náš tým</a>
+    //             <a href="#">události</a>
+    //             <a href="#">kontakt</a>
+    //         </nav>
+    //     </header>
+    // `;
 
     return `
         <header>
@@ -8,8 +22,21 @@ export const Header = (props) => {
             <nav>
             ${ links
                 .map(oneLink => `<a href="#">${oneLink}</a>`)
-                .join('') }
+                .join('')}
             </nav>
         </header>
     `;
+
+    // return `
+    //     <header>
+    //         <h1 class="site-title">${title}</h1>
+    //         <nav>
+    //             <a href="#">${links}</a>
+    //             <a href="#">${links}</a>
+    //             <a href="#">${links}</a>
+    //             <a href="#">${links}</a>
+    //             <a href="#">${links}</a>
+    //         </nav>
+    //     </header>
+    // `;
 }
